241c9b5579565b172246a4cad7901556d481fa77
Steven Le <stevenle08@gmail.com>
2014-01-26 19:38:10 +0000

    Cleaned up vim setup.

--- linux/setup.sh
+++ linux/setup.sh
@@ -9,7 +9,7 @@ sudo apt-get install git-core golang tmux vim
 cd "$(dirname "$0")"
 cwd=$(pwd -P)
 
-cp \
+ln -s \
   $cwd/.bash_aliases \
   $cwd/.bashrc \
   $cwd/.gitconfig \
@@ -19,9 +19,21 @@ cp \
   $HOME
 
 # Set up vim.
-cp -R $cwd/.vim $HOME
+mkdir -p \
+  $HOME/.vim/autoload \
+  $HOME/.vim/backup \
+  $HOME/.vim/bundle \
+  $HOME/.vim/swap \
+  $HOME/.vim/undo
+ln -s \
+  $cwd/.vim/skel \
+  $cwd/.vim/snippets \
+  $HOME/.vim
 curl -Sso ~/.vim/autoload/pathogen.vim \
   https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+cd $HOME/.vim/bundle
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/jnwhiteh/vim-golang.git
 
 # Set up go.
 mkdir -p $HOME/go/src
