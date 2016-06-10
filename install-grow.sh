f4d6402c1758deacb53f1a9e0a4d79f31c08a8a2
Steven Le <stevenle@users.noreply.github.com>
2016-06-09 18:42:52 -0700

    Update grow github path

--- install-grow.sh
+++ install-grow.sh
@@ -8,9 +8,9 @@ set -e
 platform=Mac
 
 output_dir=$HOME/bin
-grow_version=$(curl -silent https://api.github.com/repos/grow/pygrow/releases | grep tag_name | head -n 1 | cut -d '"' -f4)
+grow_version=$(curl -silent https://api.github.com/repos/grow/grow/releases | grep tag_name | head -n 1 | cut -d '"' -f4)
 grow_zip_filename=Grow-SDK-$platform-$grow_version.zip
-grow_download_url=https://github.com/grow/pygrow/releases/download/$grow_version/Grow-SDK-$platform-$grow_version.zip
+grow_download_url=https://github.com/grow/grow/releases/download/$grow_version/Grow-SDK-$platform-$grow_version.zip
 
 wget -O /tmp/$grow_zip_filename $grow_download_url
 unzip /tmp/$grow_zip_filename -d $output_dir
