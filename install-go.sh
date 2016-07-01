bee16f1345ad0354df462fd134d699560c597b08
Steven Le <stevenle@users.noreply.github.com>
2016-07-01 12:06:29 -0700

    Update to latest golang

--- install-go.sh
+++ install-go.sh
@@ -16,9 +16,11 @@
 
 set -ex
 
+GO_VERSION=1.6.2
+
 cd /tmp
-wget https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz
-tar xvzf go1.5.1.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz
+tar xvzf go${GO_VERSION}.linux-amd64.tar.gz
 sudo mv go /usr/local/go
 
 cd $HOME/.vim/bundle
